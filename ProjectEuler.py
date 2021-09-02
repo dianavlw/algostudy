bbccf24e0c84823bce74a5b01729bda389189006
dianavlw <dianavlw@gmail.com>
2021-09-02 10:08:11 -0700

    Update ProjectEuler.py

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -324,5 +324,91 @@ print(largest_so_far)
 23514624000
                       
                       
+Problem 9. Special Pythagorean triplet
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.                     
+                      
+Solutions:
+                      
+                      
+                   
+# python
+for c in range(1000):
+    for b in range(c):
+        for a in range(b):
+            if c**2 == a**2 + b**2 and a + b + c == 1000:
+                print(a*b*c)     
+                      
+                      
+                   
+for a in range(1,1000):
+    for b in range(1,1000):
+        c=1000-a-b        
+        if a**2+b**2==c**2:
+                k=int(a*b*c)
+                break
+print(k)                      
+                      
+                      
+                   
+def main():
+  sum_total = 1000
+
+  for c in range(sum_total):                      
+    for b in range(c):                      
+      for a in range(b):               
+        if a + b + c == sum_total:            
+            if is_triplet(a,b,c):         
+                  #print(a,b,c)      
+                #print(a*b*c)     
+                        
+def is_triplet(a,b,c):
+  if(a**2 + b**2) == c**2:
+    print('True', a,b,c)               
+    return(True)               
+  else:                  
+    return(False)            
+main()                      
+Answer:  31875000
+   
+                      
+                      
+                      
+Problem 10. Summation of primes
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.                     
+
+                      
+
+def isprime(n):
+    if n < 2: return False
+    if n < 4: return True
+    if n%2 == 0 or n%3 == 0: return False
+    for i in range(2,n):
+        if n%i == 0: return False
+        if i*i > n: return True
+
+def nth_prime(n):
+    pn = 0
+    list_of_primes = []
+    i = 1
+    while pn < n:
+        if isprime(i):
+            pn += 1
+            solution = i
+        i += 1
+    return solution
+
+print sum((nth_prime(10))                      
+                      
+                      
+                      
+                      
+                      
                       
                       
