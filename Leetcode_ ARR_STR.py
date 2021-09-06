1103f264dec4aacf34a69b449d38e6ac30066a3e
dianavlw <dianavlw@gmail.com>
2021-09-06 10:52:06 -0700

    090521

--- Leetcode_ ARR_STR.py
+++ Leetcode_ ARR_STR.py
@@ -327,12 +327,134 @@ num1 and num2 consist of digits only.
 Both num1 and num2 do not contain any leading zero, except the number 0 itself.
 
 """
+--------------------------------------- Solution  -------------------------------------------
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+#SOLUTION 2        
+        if "0" in [num1, num2]:
+            return "0"
+        
+#         len1 = len(num1)
+#         len2 = len(num2)
+        
+        res = [0] * (len(num1) + len(num2))
+        num1, num2 = num1[::-1], num2[::-1]
+        
+        for i1 in range(len(num1)):
+            for i2 in range(len(num2)):
+                digit = int(num1[i1]) * int(num2[i2])
+                res[i1 + i2] += digit
+                res[i1 + i2 + 1] += (res[i1 + i2] // 10)
+                res[i1 + i2] = res[i1 + i2] % 10 
+        
+        res, beg = res[::-1], 0
+        while beg < len(res) and res[beg] == 0:
+            beg += 1
+            
+        res = map(str, res[beg:])
+        return "".join(res)
+        
+# SOLUTION 2. 
+#takes more memory wont run fast
+#         len1 = len(str1)
+#         len2 = len(str2)
+        
+#         str1= list(map(int, reversed(str1)))
+#         str2= list(map(int, reversed(str2)))
+#         #[8,7] [7,5]
+        
+#         res = [0 for i in range(len1 + len2)]
+#         #[0,0,0,0]
+        
+#         for j in range(len2):
+#             for i in range(len1):
+#                 res[i+j] = res[i+j] + str[i]*str[j]
+#                 res[i+j+1] = res[i+j+1] + res[i+j] // 10 #carry
+#                 res[i+j] = res[i+j] % 10 
+                
+#         return "".join(map(str,res[::-1]))
 
 
 
 
+--------------------------------------- Rotate Image  -------------------------------------------
+"""
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. 
+DO NOT allocate another 2D matrix and do the rotation
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+"""
+
+--------------------------------------- Solution  -------------------------------------------
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l = 0
+        r = len(matrix) -1
+        while l < r:
+            matrix[l], matrix[r] = matrix[r], matrix[l]
+            l += 1
+            r -= 1
+        # transpose 
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+#SOLUTION 2
+
+# reverse: https://leetcode.com/problems/rotate-image/discuss/842087/Easy-Python-from-scratch-(2-Steps)
+#Transposing means: rows become columns, columns become rows.
+
+
+l = 0
+r = len(matrix) -1
+while l < r:
+	matrix[l], matrix[r] = matrix[r], matrix[l]
+	l += 1
+	r -= 1
+# transpose 
+for i in range(len(matrix)):
+	for j in range(i):
+		matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+#SOLUTION 3  : https://www.youtube.com/watch?v=ymOBPRt9UR0
+N = len(matrix)
+
+for r in range(N):
+  for c in range(r):
+    matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+for r in matrix:
+    r.reverse()
+OR
+
+N = len(matrix)
+matrix.reverse()
+for r in range(N):
+  for c in range(r):
+    matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+    
+    
+--------------------------------------- Problem  -------------------------------------------
+
+--------------------------------------- Solution  -------------------------------------------
 
 
 
+--------------------------------------- Problem  -------------------------------------------
+
+--------------------------------------- Solution  -------------------------------------------
+
+
+
+--------------------------------------- Problem  -------------------------------------------
+
+--------------------------------------- Solution  -------------------------------------------
 
         
