6449807f6f585698fb5effabd89acc6a9d15ba83
dianavlw <dianavlw@gmail.com>
2021-09-08 22:28:31 -0700

    Create Depth_First_Search_AlgoExp.py

---
+++ Depth_First_Search_AlgoExp.py
@@ -0,0 +1,20 @@
+----------PROBLEM------------
+"""
+Algo Expert: Depth First Search:https://www.algoexpert.io/questions/Depth-first%20Search
+"""
+---------SOLUTION------------
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+		for child in self.children:
+			child.depthFirstSearch(array)
+		return array
+
