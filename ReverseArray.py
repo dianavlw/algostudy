374124722f5137990cbee8dfbefedeb904d953a4
dianavlw <dianavlw@gmail.com>
2021-09-08 21:14:54 -0700

    Create ReverseArray.py

---
+++ ReverseArray.py
@@ -0,0 +1,6 @@
+https://www.hackerrank.com/domains/data-structures?filters%5Bsubdomains%5D%5B%5D=arrays
+  
+-------------------------------------------- Arrays - DS. -------------------------------------------- 
+def reverseArray(a):
+    # Write your code here
+    return a[::-1]
