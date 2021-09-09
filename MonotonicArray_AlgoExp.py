ce837081a1ecc404df32dc265de8b19d43a9e554
dianavlw <dianavlw@gmail.com>
2021-09-09 09:33:36 -0700

    Update MonotonicArray_AlgoExp.py

--- MonotonicArray_AlgoExp.py
+++ MonotonicArray_AlgoExp.py
@@ -16,7 +16,8 @@ Non-increasing elements arent necessarily exclusively decreasin they simply dont
 necessarily exclusively increasing, they simply dont decrease.
 """
 
-
+# SOLUTION 1:
+	
 def isMonotonic(array):
 	if len(array) <= 2:
 		return True
@@ -35,6 +36,16 @@ def breaksDirection(direction, previousInt, currentInt):
 		return difference < 0
 	return difference > 0
 
+# SOLUTION 2:
+def isMonotonic(array):
+    isNonDecreasing = True
+	isNonIncreasing = True
+	for i in range(1, len(array)):
+		if array[i] < array[i-1]:
+			isNonDecreasing = False
+		if array[i] > array[i-1]:
+			isNonIncreasing = False
+	return isNonDecreasing or isNonIncreasing 
 
 
 
