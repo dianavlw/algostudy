6deed1382d08756102895d0ffa0781e85fa5e320
dianavlw <dianavlw@gmail.com>
2021-09-08 21:44:13 -0700

    Update ContainerWithMostWater.py

--- ContainerWithMostWater.py
+++ ContainerWithMostWater.py
@@ -3,7 +3,7 @@
 
 -------------------------- Container with most Water --------------------------
 """
-Container With Most Water
+Container With Most Water : https://leetcode.com/problems/container-with-most-water/
 
 Solution
 Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
