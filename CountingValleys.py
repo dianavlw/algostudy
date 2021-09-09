49ba830bbe280e6b9bbfe8ace389bee5411c013f
dianavlw <dianavlw@gmail.com>
2021-09-08 21:30:02 -0700

    Create CountingValleys.py

---
+++ CountingValleys.py
@@ -0,0 +1,36 @@
+
+"""
+COUNTING VALLEYS
+An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
+A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+Sample Input: 
+8
+UDDDUDUU
+ 
+Sample Output:
+1
+"""
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    count = 0
+    res = 0
+    
+    for c in path:
+        if c == 'U':
+            count += 1
+        else: 
+            count -=1
+        if count == 0 and c == 'U': 
+            res += 1
+    return res
