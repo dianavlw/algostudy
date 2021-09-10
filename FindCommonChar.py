b142ca51a89e1914166b34af95d92e65e9dd9371
dianavlw <dianavlw@gmail.com>
2021-09-09 23:24:59 -0700

    Update FindCommonChar.py

--- FindCommonChar.py
+++ FindCommonChar.py
@@ -14,34 +14,26 @@ Input: words = ["cool","lock","cook"]
 Output: ["c","o"]
 """
 
-# Solutions are references from leetcode, this is how i like to review by viewing others work and applying to my owncode.
+# Solutions are references from leetcode
 # Solution 1.
 class Solution:
-    def commonChars(self, words: List[str]) -> List[str]:
-        result = []
-            for c in set(A[0]):
-                count = A[0].count(c)
-                occurences = 1
-                for i in range(1,len(A)):
-                    if c in A[i]:
-                        count = min(count,A[i].count(c))
-                        occurences += 1
-                    else:
-                        break
-                if occurences == len(A):
-                    for i in range(count):
-                        result.append(c)
+    def commonChars(self, n: List[str]) -> List[str]:
+        l =len(n)
+        d = {}
 
-            return result
-# Solution 2.
-def commonChars(self,A):
-        check = list(A[0])
-        for word in [A:]:
-            newCheck = []
-            for c in word:
-                if c in check:
-                    newCheck.append(c)
-                    check.remove(c)
-            check = newCheck
-        
-        return check
+        for x in n:
+            for y in set(x):
+                if y not in d:
+                    d[y] = [x.count(y)]
+                else:
+                    d[y].append(x.count(y))
+        res = []
+        for x, y in d.items():
+            if len(y) >= l:
+                z = min(y)
+                if z > 1:
+                    for _ in range(z):
+                        res.append(x)
+                else:
+                    res.append(x)
+        return(res)
