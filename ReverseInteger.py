09b2b01bd6b8dae4c51564ebbd4217aeb4c05860
dianavlw <dianavlw@gmail.com>
2021-09-14 19:07:06 -0700

    Update ReverseInteger.py

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -10,6 +10,18 @@ Example 1:
 
 Input: x = 123
 Output: 321
+
+
+123 % 10 = 3
+12%10= 2
+1%10 =1
+
+3 * 10 = will shift to the left by 1
+
+remainder = x % 10 
+sum = sum * 10 + rem
+x = x //10
+
 """
 
 class Solution(object):
@@ -30,3 +42,20 @@ class Solution(object):
         if not -2**31 < sum < 2**31-1:
             return 0
         return sign*sum
+# SOLUTION 2. 
+
+    def reversed(self, x):
+        sum = 0
+        if x < 0:
+            sign = -1
+            x = -x
+        else:
+            sign = 1
+        
+        while x > 0:
+            rem = x % 10
+            sum = sum * 10 + rem
+            x //= 10
+        if not -2147483648<sum<2147483647:
+             return 0
+        return sign * sum 
