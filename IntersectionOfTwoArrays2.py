1ab5d4b95e38221393c72b9280d43ade5bdbb1fe
dianavlw <dianavlw@gmail.com>
2021-09-15 10:28:34 -0700

    Update IntersectionOfTwoArrays2.py

--- IntersectionOfTwoArrays2.py
+++ IntersectionOfTwoArrays2.py
@@ -69,4 +69,19 @@ class Solution:
                 dictionary[n] = dictionary[n] - 1
                 
         return array
-                
+#SOlUTION 3. 
+        nums1.sort()
+        nums2.sort()
+        res = []
+        a = 0
+        b = 0
+        while a < len(nums1) and b < len(nums2):
+            if nums1[a] == nums2[b]:
+                res.append(nums1[a])
+                a += 1
+                b += 1
+            elif nums1[a] < nums2[b]:
+                a += 1
+            else:
+                b += 1
+        return res
