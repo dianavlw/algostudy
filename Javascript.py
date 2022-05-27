b2f7d104c8bfc90f455241f43d78c0bab756a6c5
Diana Vargas <dianavlw@gmail.com>
2022-05-27 09:07:28 -0700

    Create Javascript.py

---
+++ Javascript.py
@@ -0,0 +1,11 @@
+DOM
+
+-https://www.freecodecamp.org/news/dom-explained-everything-you-need-to-know-about-the-document-object-model/
+-https://www.linkedin.com/learning/javascript-essential-training/access-elements-with-queryselector-methods?autoplay=true&resume=false
+  
+  
+BIG O NOTATION
+https://www.freecodecamp.org/news/learn-big-o-notation/
+  
+
+ 
