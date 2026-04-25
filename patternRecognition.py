fb0739b9de8eff1c8c08840de9fc3562e9b26455
Diana Vargas <dianavlw@gmail.com>
2026-04-25 07:16:54 -0700

    Update patternRecognition.py

--- patternRecognition.py
+++ patternRecognition.py
@@ -192,7 +192,26 @@ def subarraySum(nums, k):
         
         
     
+def maxSubArray(nums):
+    cur_sum = 0
+    max_sum = float('-inf)
+    n = len(nums)
 
+    for i in range(len(n)):
+        cur_sum += nums[i]
+        max_sum = max(max_sum, cur_sum)
+        if cur_sum < 0:
+            cur_sum = 0
+    return max_sum 
+    
+
+
+
+
+
+
+
+    
     
 
 
