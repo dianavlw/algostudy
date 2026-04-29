7c4a52528227e2060a66fe8d624c76b3f96d2235
Diana Vargas <dianavlw@gmail.com>
2026-04-29 10:01:55 -0700

    Update easy75.py

--- easy75.py
+++ easy75.py
@@ -19,7 +19,18 @@ class Solution:
 
 
       
+#1071. Greatest Common Divisor of Strings
 
+class Solution:
+    def gcdOfStrings(str1, str2):
+        if str1 + str2 != str2 + str1
+            return ""
+        a, b = len(str1), len(str2)
+
+        while b > 0:
+            a, b = b , a % b
+            
+        return str1[:a]
 
         
       
