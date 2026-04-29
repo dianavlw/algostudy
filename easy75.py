00d0ba61dc4eb440e6550d890eb6d17c880b85d7
Diana Vargas <dianavlw@gmail.com>
2026-04-29 10:07:27 -0700

    Update test easy75.py

--- easy75.py
+++ easy75.py
@@ -4,18 +4,25 @@ Strings
 #1768. Merge strings alternately 
 
 class Solution:
-    def mergeAlternately(word1, word2):
-      res = []
-      i, j = 0,0
+    def mergeAlternately(self, word1, word2):
+        res = []
+        i, j = 0, 0
 
-      for i < len(word1)) and j < len(word2)):
-          res.append(word1[i])
-          res.append(word2[i])
-          i += 1
-          j += 1
-      res.append(word1[i:])
-      res.append(word2[j:])
-      return ''.join(res)
+        while i < len(word1) and j < len(word2):
+            res.append(word1[i])
+            res.append(word2[j])
+            i += 1
+            j += 1
+
+        res.append(word1[i:])
+        res.append(word2[j:])
+
+        return ''.join(res)
+
+
+
+data = Solution()
+print(data.mergeAlternately("ABCDEF", "GHIJK"))  # Expected: "AGBHCIDJEKF"
 
 
       
